cd8686e397865bc216022c205fdb1036b7c0c443
Vinci Wu <vinciwu.vwu@gmail.com>
2025-07-12 23:29:45 -0400

    Created token.go to hold token data structure definition and token type constants

---
+++ src/monkey/token/token.go
@@ -0,0 +1,40 @@
+package token
+
+/*
+	Token data structure definition
+*/
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+/*
+	Token type constants
+*/
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifiers & literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	//	delimiters
+	COMMA     = ","
+	SEMICOLON = ":"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
