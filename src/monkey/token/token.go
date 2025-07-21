7864443f0bfd3326942328c4701a79972ed757cb
Vinci Wu <vinciwu.vwu@gmail.com>
2025-07-20 22:33:12 -0400

    Fixed SEMICOLON type from : -> ;. Added new types for equal (EQ) and not equal (NOT_EQ) as well as true, false, if, else, and return.

--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -22,9 +22,12 @@ const (
 	LT = "<"
 	GT = ">"
 
+	EQ     = "=="
+	NOT_EQ = "!="
+
 	//	delimiters
 	COMMA     = ","
-	SEMICOLON = ":"
+	SEMICOLON = ";"
 
 	LPAREN = "("
 	RPAREN = ")"
@@ -34,6 +37,11 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 /*
@@ -47,10 +55,21 @@ type Token struct {
 }
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
+/*
+	if a string is a keyword,
+		return the type,
+	else
+		return the identity
+*/
 func LookupIdent(ident string) TokenType {
 	if tok, ok := keywords[ident]; ok {
 		return tok
